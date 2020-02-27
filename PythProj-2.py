25ba1f9911a30a5574aaeed219badbbe29f303a1
JackVanDongen <jackvandongen@outlook.com>
2020-02-27 18:08:03 +0100

    Commit 4.

--- PythProj-2.py
+++ PythProj-2.py
@@ -1,6 +1,13 @@
 import time
 
-def foo(a, b):
-    """Foo function does foo things with a and b!"""
-    print('{0} and {1} are really fun!'.format(a,b))
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+while True:
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(1.0)
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(1.0)
