3943ff6c3e3863621d8bedbafdfc61cfd94fd6ed
JackVanDongen <jackvandongen@outlook.com>
2020-02-28 11:36:24 +0100

    Changed PWM frequency and step size to get smoother dimming.

--- PythProj-2.py
+++ PythProj-2.py
@@ -9,14 +9,14 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+p = GPIO.PWM(12, 500)  # channel=12 frequency=50Hz
 p.start(0)
 try:
     while 1:
-        for dc in range(0, 101, 5):
+        for dc in range(0, 101, 1):
             p.ChangeDutyCycle(dc)
             time.sleep(0.1)
-        for dc in range(100, -1, -5):
+        for dc in range(100, -1, -1):
             p.ChangeDutyCycle(dc)
             time.sleep(0.1)
 except KeyboardInterrupt:
