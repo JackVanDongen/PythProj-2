66e795a3339ee5467e963f6344bb9f7b6c7d1754
JackVanDongen <jackvandongen@outlook.com>
2020-02-28 11:10:48 +0100

    Commit 1 on PWM branch - Flashing LED exeample from sourgeforge.net

--- PythProj-2.py
+++ PythProj-2.py
@@ -1,13 +1,13 @@
-import time
+# The following code is copied from: https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/ 
+# The program flashes a LED using the GPIO.PWM fonction 
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
 
-GPIO.setup(18, GPIO.OUT)
-
-while True:
-    GPIO.output(18, GPIO.LOW)
-    time.sleep(1.0)
-    GPIO.output(18, GPIO.HIGH)
-    time.sleep(1.0)
+p = GPIO.PWM(12, 0.5)
+p.start(1)
+input('Press return to stop:')   # use raw_input for Python 2
+p.stop()
+GPIO.cleanup()
